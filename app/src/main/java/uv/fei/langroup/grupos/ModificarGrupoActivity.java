e91119a350e9adee2a5f5a19248fef240611e7dc
Luis C <zs21013875@estudiantes.uv.mx>
2024-05-23 14:27:40 -0600

    Ventanas para publicacion y modificar grupo

---
+++ app/src/main/java/uv/fei/langroup/grupos/ModificarGrupoActivity.java
@@ -0,0 +1,16 @@
+package uv.fei.langroup.grupos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import uv.fei.langroup.R;
+
+public class ModificarGrupoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_grupo);
+    }
+}
