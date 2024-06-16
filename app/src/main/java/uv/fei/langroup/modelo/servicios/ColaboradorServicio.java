1e063581b747d85b753eca5b551194ee233fca04
René García <100447765+GODZ255GG@users.noreply.github.com>
2024-06-16 02:21:20 -0600

    Commit crear cuenta

--- app/src/main/java/uv/fei/langroup/modelo/servicios/ColaboradorServicio.java
+++ app/src/main/java/uv/fei/langroup/modelo/servicios/ColaboradorServicio.java
@@ -1,4 +1,14 @@
 package uv.fei.langroup.modelo.servicios;
 
-public class ColaboradorServicio {
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+import uv.fei.langroup.modelo.POJO.Colaborador;
+
+public interface ColaboradorServicio {
+
+    @POST("colaboradores")
+    Call<Colaborador> crearCuenta(@Body Map<String, Object> credentials);
 }
--- app/src/main/java/uv/fei/langroup/vista/cuenta/CrearCuentaActivity.java
+++ app/src/main/java/uv/fei/langroup/vista/cuenta/CrearCuentaActivity.java
@@ -2,11 +2,19 @@ package uv.fei.langroup.vista.cuenta;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import uv.fei.langroup.MainActivity;
 import uv.fei.langroup.databinding.ActivityCrearCuentaBinding;
+import uv.fei.langroup.modelo.DAO.ColaboradorDAO;
+import uv.fei.langroup.modelo.POJO.Colaborador;
 
 public class CrearCuentaActivity extends AppCompatActivity {
 
@@ -21,5 +29,65 @@ public class CrearCuentaActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         });
+
+        EditText nombres = binding.edtNombres;
+        EditText apellidos = binding.editApellidos;
+        EditText usuario = binding.edtUsuario;
+        EditText correo = binding.editCorreo;
+        EditText contrasenia = binding.editContrasena;
+        EditText repitContrasenia = binding.editConfirmarContrasena;
+
+        binding.btnCrear.setOnClickListener(v -> {
+            String nombresCrear = nombres.getText().toString();
+            String apellidosCrear = apellidos.getText().toString();
+            String usuarioCrear = usuario.getText().toString();
+            String correoCrear = correo.getText().toString();
+            String contraseniaCrear = contrasenia.getText().toString();
+            String repiteContra = repitContrasenia.getText().toString();
+
+            if(!camposVacios(nombresCrear, apellidosCrear, usuarioCrear, correoCrear, contraseniaCrear, repiteContra)){
+                Log.d("CrearCuenta", nombresCrear);
+                Log.d("CrearCuenta", apellidosCrear);
+                Log.d("CrearCuenta", usuarioCrear);
+                Log.d("CrearCuenta", correoCrear);
+                Log.d("CrearCuenta", contraseniaCrear);
+                crearCuenta(nombresCrear, apellidosCrear, usuarioCrear, correoCrear, contraseniaCrear);
+            }else{
+                showMessage("Ingrese datos para poder crear la cuenta");
+            }
+        });
+    }
+
+    private boolean camposVacios(String nombre, String apellido, String usuario, String correo, String contrasenia, String repitContrasenia){
+        if(nombre.isEmpty() || apellido.isEmpty() || usuario.isEmpty() || correo.isEmpty() || contrasenia.isEmpty() || repitContrasenia.isEmpty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private void crearCuenta(String nombre, String apellido, String usuario, String correo, String contrasenia){
+        Log.d("CrearCuenta", "Antes de llamar al método crearCuenta del DAO");
+        ColaboradorDAO.crearCuenta(nombre, apellido, usuario, correo, contrasenia, new Callback<Colaborador>() {
+            @Override
+            public void onResponse(Call<Colaborador> call, Response<Colaborador> response) {
+                Colaborador colaborador = response.body();
+                if(colaborador != null){
+                    showMessage("Cuenta Creada exitosamente");
+                }else{
+                    showMessage("Operación no completa, intente más tarde");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Colaborador> call, Throwable t) {
+                t.printStackTrace();
+                showMessage("Error en la conexión");
+            }
+        });
+    }
+
+    public void showMessage(String msj){
+        Toast.makeText(this, msj, Toast.LENGTH_SHORT).show();
     }
 }
