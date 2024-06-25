097333ff69f8bbf58eadba3c00ef34b67049bc48
luis_cvqz <zs21013875@estudiantes.uv.mx>
2024-06-24 21:47:03 -0600

    Quitar codigo sin usar, nuevos imports & usar databinding

--- app/src/main/java/uv/fei/langroup/vista/publicaciones/AgregarPublicacionFragment.java
+++ app/src/main/java/uv/fei/langroup/vista/publicaciones/AgregarPublicacionFragment.java
@@ -1,72 +1,61 @@
 package uv.fei.langroup.vista.publicaciones;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import java.util.ArrayList;
 
 import uv.fei.langroup.R;
+import uv.fei.langroup.databinding.FragmentAgregarPublicacionBinding;
+import uv.fei.langroup.modelo.POJO.Grupo;
+import uv.fei.langroup.utilidades.SesionSingleton;
+import uv.fei.langroup.vistamodelo.publicaciones.AgregarPublicacionViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AgregarPublicacionFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AgregarPublicacionFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public AgregarPublicacionFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AgregarPublicacionFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AgregarPublicacionFragment newInstance(String param1, String param2) {
-        AgregarPublicacionFragment fragment = new AgregarPublicacionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+    FragmentAgregarPublicacionBinding binding;
+    private AgregarPublicacionViewModel viewModel;
+    ActivityResultLauncher<Intent> resultLauncher;
+    private Uri imagenPublicacionURI;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_agregar_publicacion, container, false);
+        binding = FragmentAgregarPublicacionBinding.inflate(getLayoutInflater());
+        View root = binding.getRoot();
 
-        final ImageButton buttonRegresar = root.findViewById(R.id.button_regresar);
+        viewModel = new ViewModelProvider(this).get(AgregarPublicacionViewModel.class);
+
+        final ImageButton buttonRegresar = binding.buttonRegresar;
+        final Spinner spinnerGrupos = binding.spinnerGrupos;
+        final EditText editTextTituloPublicacion = binding.editTextTituloPublicacion;
+        final Button buttonSeleccionarImagen = binding.buttonSeleccionarImagen;
+        final ImageView imageViewArchivo = binding.imageViewArchivo;
+        final EditText editTextDescripcionPublicacion = binding.editTextDescripcionPublicacion;
+        final Button buttonPublicar = binding.buttonPublicar;
+
+        llenarSpinner(spinnerGrupos);
 
         buttonRegresar.setOnClickListener(new View.OnClickListener() {
             @Override
